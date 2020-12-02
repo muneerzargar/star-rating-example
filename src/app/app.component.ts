28398a394b1f882c2828dfb66ab8e08b7f375efc
Muneer Zargar <zargarmuneer@yahoo.com>
2020-12-02 12:55:28 +0530

    star rating
    * Application Setup.
    * Creation of interface.
    * Added click event on each star item.
    * CSS for star item.
    * On click extract the item till selected item index.
    * on each item of the list add selected as true.
    * Update the starList by merging the list and starList using the Set to remove duplicates.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IStar } from './starInterface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'star-rating';
+  ratingValue = 0;
+  starList: IStar[] = [{
+    id: 1
+  },
+  {
+    id: 2
+  },
+  {
+    id: 3
+  },
+  {
+    id: 4
+  }
+];
+
+  onStarClicked( item: IStar) {
+    this.ratingValue = this.starList.findIndex((star) => star.id === item.id) + 1;
+    const list = this.starList.slice(0, this.ratingValue);
+    list.forEach((value) => value.selected = true);
+    this.starList = [...new Set([].concat([...list, ...this.starList]))];
+  }
 }
---
+++ src/app/starInterface.ts
@@ -0,0 +1,4 @@
+export interface IStar {
+    id: number;
+    selected?: boolean;
+}
